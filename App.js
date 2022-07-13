8b77e05badc6b61a2d254f68a097435157bfa715
rainawan <rainawan02@gmail.com>
2022-07-13 15:57:04 -0700

    store messages in firebase

--- App.js
+++ App.js
@@ -3,21 +3,17 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat'
 import db from "./firebase";
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, doc, updateDoc, setDoc } from 'firebase/firestore';
 
 export default function App() {
 
   const [messages, setMessages] = useState([]);
 
-  // useEffect(() => {
-  //   db.collection("Chats")
-  //     .doc("myfirstchat")
-  //     .get()
-  //     .then((snapshot) => {
-  //       console.log(snapshot.id);
-  //       console.log(snapshot.data());
-  //     });
-  // }, []);
+
+  // const chatsRef = doc(db, "Chats");
+  // await updateDoc(collectionRef, {
+  //   capital: true
+  // })
 
 
   useEffect(() => {
@@ -33,54 +29,20 @@ export default function App() {
     getChat();
   }, []);
 
+  
+  const onSend = useCallback(async (messages = []) => {
 
-  useEffect(() => {
-
-    setMessages(history.reverse())
-  }, [])
-
-  let history = [
-    {
-      _id: 1,
-      text: 'Hello Bull',
-      createdAt: new Date(),
-      user: {
-        _id: 2,
-        name: 'Poco',
-        avatar: 'https://static.wikia.nocookie.net/brawlstars/images/2/24/Poco_Skin-Default.png/revision/latest?cb=20211112215606',
-      },
-    },
-    {
-      _id: 1,
-      text: 'How it goes',
-      createdAt: new Date(),
-      user: {
-        _id: 2,
-        name: 'Poco',
-        avatar: 'https://static.wikia.nocookie.net/brawlstars/images/2/24/Poco_Skin-Default.png/revision/latest?cb=20211112215606',
-      },
-    },
-
-    {
-      _id: 3,
-      text: 'Goodbye',
-      createdAt: new Date(),
-      user: {
-        _id: 4,
-        name: 'Bull',
-        avatar: 'https://static.wikia.nocookie.net/brawlstars/images/b/b7/Bull_Skin-Default.png/revision/latest/scale-to-width-down/1200?cb=20210913081903',
-      },
-    },
-  ]
-
-  const onSend = useCallback((messages = []) => {
+    await setDoc(doc(db, "Chats", "myfirstchat"), {
+      messages: messages
+    });
     setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
-  }, [])
+}, [])
 
   return (
 
     <GiftedChat
       messages={messages}
+      
       onSend={messages => onSend(messages)}
       user={{
         _id: 1,
