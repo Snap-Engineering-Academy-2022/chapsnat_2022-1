75163853a8d0cb2aceb16612eda5db724535d54e
rainawan <rainawan02@gmail.com>
2022-07-20 14:26:02 -0700

    testing

--- utils/hooks/useAuthentication.js
+++ utils/hooks/useAuthentication.js
@@ -1,17 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+import { doc, getDoc, collection, onSnapshot, arrayUnion, updateDoc } from "firebase/firestore";
+// import db from "./firebase";
 
 const auth = getAuth();
+// const F6gfFNT5SBcaek0zlbklOrfMLmE2 = doc(db, 'Users', 'F6gfFNT5SBcaek0zlbklOrfMLmE2')
+// const Users = collection(db, 'Users');
+
+const docRef = doc(db, "Users", "F6gfFNT5SBcaek0zlbklOrfMLmE2", "name");
+const docSnap = await getDoc(docRef);
 
 export function useAuthentication() {
 	const [user, setUser] = useState();
+	const [userData, setUserData] = useState();
 
 	useEffect(() => {
 		const unsubscribeFromAuthStatusChanged = onAuthStateChanged(auth, (user) => {
 		if (user) {
 			// User is signed in, see docs for a list of available properties
 			// https://firebase.google.com/docs/reference/js/firebase.User
-			setUser(user);
+
+			getDoc().then((docSnap) => {
+				setUserData(userData);
+				console.log("userData", userData)
+			})
+
+			console.log("Document data:", docSnap.data());
+
+			// setUser(user);
 		} else {
 			// User is signed out
 			setUser(undefined);
